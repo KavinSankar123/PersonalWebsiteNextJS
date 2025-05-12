c4787b19fae038712672a111cab7e072004493df
KavinSPitt <kavin.sankar@gmail.com>
2025-05-12 10:37:53 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -25,9 +25,7 @@ export default function Home() {
       {isDesktop ? (
         <div className="flex justify-between max-w-4xl w-full p-4">
           <div className="w-1/3 p-4">
-            {/* <div className="max-w-64 text-slate-200 fixed"> */}
-            {/* <div className="max-w-96 text-slate-200 fixed"> */}
-            <div className="max-w-[1rem] text-slate-200 fixed">
+            <div className="max-w-64 text-slate-200 fixed">
               <h1
                 id="typewriter-animate1"
                 className="font-sans font-bold text-4xl mt-9"
