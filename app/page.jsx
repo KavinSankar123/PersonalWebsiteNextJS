63be3b318a6b2daba367ee06d9dafd24d626eee9
KavinSPitt <kavin.sankar@gmail.com>
2025-05-12 10:34:11 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -27,7 +27,7 @@ export default function Home() {
           <div className="w-1/3 p-4">
             {/* <div className="max-w-64 text-slate-200 fixed"> */}
             {/* <div className="max-w-96 text-slate-200 fixed"> */}
-            <div className="max-w-[1rem] text-slate-200 fixed">
+            <div className="text-slate-200 fixed">
               <h1
                 id="typewriter-animate1"
                 className="font-sans font-bold text-4xl mt-9"
